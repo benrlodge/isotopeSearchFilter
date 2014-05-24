2fa20a30f120e8d064131dbf291b0bb92d9aa71c
Ben Lodge <blodge@hubspot.com>
2014-05-23 20:49:39 -0400

    organize code

--- javascripts/searchfilter.jquery.coffee
+++ javascripts/searchfilter.jquery.coffee
@@ -3,56 +3,83 @@ log = (m) ->
 
 
 
-$ = jQuery
-$.fn.extend
-
-	searchfilter: (options) ->
-	
-		# settings =
-		# 	selector	: this
-		# 	item : ''
-		# 	input : ''
-		# 	searchBtn : ''
-		# 	searchItem : ''
-
-		# settings = $.extend settings, options
-
-		# log = (msg) ->
-		# 	console.log msg
-
-
-		# return @each () ->
-
-		# 	selector = settings.selector
-		# 	item = settings.item
-		# 	input = settings.input
-		# 	searchBtn = settings.searchBtn
-		# 	item_name = settings.searchItem
+$(document).ready -> BL_Searchable.init()
 
 
 
-	allNames = []
-	Input = $('#searchInput')
-	
-	searchBtn = $('#search-btn')
-	$item_name = $('.item-name')
+BL_Searchable = 
+
+	input 		:	$('#searchInput')
+	tags 		:	$('.tags')
+	item_name	:	$('.item-name')
+
+	all_tags 	: 	''  # change name later...
+	array_names :	[]
+	search_items:	[]
 
 
-	$item_name.each () ->
-		nameText = $(this).text()
-		allNames.push(nameText)
 
-	Input.autocomplete source: allNames
+ 
+	init: ->
+		@bindEvents()
+		@search_items = @getTags()
+		@searchTags(@search_items)
+
+
+
+
+
 		
 
-	Input.on("autocompletechange change", ->
-		typed = @value
-		console.log typed
-		$item_name.removeClass('active')
+	bindEvents: ->
+		scope = this
+		@input.keypress (e) ->
+			keyCode = e.keyCode || e.which
+			if (keyCode == 13)
+				scope.searchTags()
+			
 
-		$item_name.filter(->
-			$(this).text() is typed
-		).closest('.item').addClass('active').siblings().addClass('inactive')
 
-	).change()
-		
+	getTags: ->
+		scope = this
+
+		# Add comma to end of tag list if doesn't already have one
+		@tags.each ->
+			text = $(this).text()
+			lastChar = text.substr(text.length - 1)
+			text += ',' if lastChar != ','
+			scope.all_tags += text
+
+		# Move tags into array
+		all_tags_array = @all_tags.split(',')
+
+		# Loop through names and toss in array
+		@item_name.each ->
+			name = $(this).text()
+			scope.array_names.push(name)
+
+
+		# Combine all searchable arrays
+		return $searchable = this.array_names.concat(all_tags_array)
+
+
+
+
+	searchTags: (items) ->
+		scope = this
+
+		@input.autocomplete source: items
+			
+		@input.on("autocompletechange change", ->
+			log 'input autocompletechange'
+			typed = @value
+			scope.item_name.removeClass('active')
+			scope.item_name.removeClass('inactive')
+
+
+			scope.item_name.filter(->
+				$(this).text() is typed
+			).closest('.item').addClass('active').siblings().addClass('inactive')
+
+		).change()
+	
--- javascripts/searchfilter.jquery.js
+++ javascripts/searchfilter.jquery.js
@@ -1,28 +1,74 @@
 (function() {
-  var $, $item_name, Input, allNames, log, searchBtn;
+  var BL_Searchable, log;
 
   log = function(m) {
     return console.log(m);
   };
 
-  $ = jQuery;
+  $(document).ready(function() {
+    return BL_Searchable.init();
+  });
 
-  $.fn.extend({
-    searchfilter: function(options) {}
-  }, allNames = [], Input = $('#searchInput'), searchBtn = $('#search-btn'), $item_name = $('.item-name'), $item_name.each(function() {
-    var nameText;
-    nameText = $(this).text();
-    return allNames.push(nameText);
-  }), Input.autocomplete({
-    source: allNames
-  }), Input.on("autocompletechange change", function() {
-    var typed;
-    typed = this.value;
-    console.log(typed);
-    $item_name.removeClass('active');
-    return $item_name.filter(function() {
-      return $(this).text() === typed;
-    }).closest('.item').addClass('active').siblings().addClass('inactive');
-  }).change());
+  BL_Searchable = {
+    input: $('#searchInput'),
+    tags: $('.tags'),
+    item_name: $('.item-name'),
+    all_tags: '',
+    array_names: [],
+    search_items: [],
+    init: function() {
+      this.bindEvents();
+      this.search_items = this.getTags();
+      return this.searchTags(this.search_items);
+    },
+    bindEvents: function() {
+      var scope;
+      scope = this;
+      return this.input.keypress(function(e) {
+        var keyCode;
+        keyCode = e.keyCode || e.which;
+        if (keyCode === 13) {
+          return scope.searchTags();
+        }
+      });
+    },
+    getTags: function() {
+      var $searchable, all_tags_array, scope;
+      scope = this;
+      this.tags.each(function() {
+        var lastChar, text;
+        text = $(this).text();
+        lastChar = text.substr(text.length - 1);
+        if (lastChar !== ',') {
+          text += ',';
+        }
+        return scope.all_tags += text;
+      });
+      all_tags_array = this.all_tags.split(',');
+      this.item_name.each(function() {
+        var name;
+        name = $(this).text();
+        return scope.array_names.push(name);
+      });
+      return $searchable = this.array_names.concat(all_tags_array);
+    },
+    searchTags: function(items) {
+      var scope;
+      scope = this;
+      this.input.autocomplete({
+        source: items
+      });
+      return this.input.on("autocompletechange change", function() {
+        var typed;
+        log('input autocompletechange');
+        typed = this.value;
+        scope.item_name.removeClass('active');
+        scope.item_name.removeClass('inactive');
+        return scope.item_name.filter(function() {
+          return $(this).text() === typed;
+        }).closest('.item').addClass('active').siblings().addClass('inactive');
+      }).change();
+    }
+  };
 
 }).call(this);
